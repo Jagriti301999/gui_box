188c7c78689ba0bb3411ae94d06efe76ac102bdb
Jagriti301999 <70887862+Jagriti301999@users.noreply.github.com>
2020-10-09 13:04:40 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,5 @@
+from tkinter import *
+root=Tk()
+myLabel1=Label(root,text="hey beautiful!Welcome to this World").grid(row=0,column=0)
+myLabel2=Label(root,text="hey handsome!Welcome to this World").grid(row=1,column=1)
+root.mainloop()           
